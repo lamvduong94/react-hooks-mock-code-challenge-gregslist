4907cc00ea7db06b598ce6e447d89ea635722a1b
Lam <LamDuong.inbox@gmail.com>
2021-12-15 12:58:10 -0800

    commit tree 493c6c1053216dd53cd17e1a3ef35ca33d4748e1

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,58 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useEffect, useState } from "react";
+import ListingCard from "./ListingCard";
+import NewListingForm from "./NewListingForm";
 
-function ListingsContainer() {
+function ListingsContainer({ search }) {
+  const [listings, setListings] = useState([]);
+  const [sortBy, setSortBy] = useState("id");
+  
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then(resp => resp.json())
+      .then(data => setListings(data))
+  }, []);
+
+  function handleDelete(id) {
+    const updatedListings = listings.filter(
+      listing => listing.id !== id
+    )
+
+    setListings(updatedListings);
+  }
+
+  function handleAdd(newListing) {
+    const updatedListings = [newListing, ...listings];
+    setListings(updatedListings);
+  }
+
+  let listingCards = listings
+    .filter(listing =>
+      listing.description.toLowerCase().include(search.toLowerCase())
+    )
+
+    .sort((listingA, listingB) => {
+      if (sortBy === "id") {
+        return listingA.id - listingB.id;
+      } else {
+        return listingA.location.localeCompare(listingB.location);
+      }
+    })
+
+    .map(listingObj => {
+      <ListingCard
+        key={listingObj.id}
+        listing={listingObj}
+        onDeleteListing={handleDelete}
+      />
+    })
+
   return (
     <main>
+      <NewListingForm onAddListing={handleAdd} />
+      <button onClick={() => setSortBy("id")}>Sort By Default</button>
+      <button onClick={() => setSortBy("location")}>Sort By Location</button>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingCards}
       </ul>
     </main>
   );
